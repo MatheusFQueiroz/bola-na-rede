c6167c97b53f8d52c973ee9742ffd062156fc848
Matheus <mf5881585@gmail.com>
2026-06-08 13:33:16 -0300

    feat(ranking): add RankingModule

---
+++ bolanarede_api/services/ranking/src/modules/ranking/ranking.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { SharedModule } from '@shared/shared.module';
+import { RANKING_REPOSITORY } from './domain/repositories/ranking-repository.interface';
+import { DrizzleRankingRepository } from './infra/database/repositories/drizzle-ranking.repository';
+import { RankingMessagingService } from './application/services/ranking-messaging.service';
+import { RankingService } from './application/services/ranking.service';
+import { GameEventsConsumer } from './application/services/game-events.consumer';
+import { RankingsController } from './infra/controllers/rankings.controller';
+
+@Module({
+  imports: [SharedModule],
+  controllers: [RankingsController],
+  providers: [
+    { provide: RANKING_REPOSITORY, useClass: DrizzleRankingRepository },
+    RankingMessagingService,
+    RankingService,
+    GameEventsConsumer,
+  ],
+})
+export class RankingModule {}
